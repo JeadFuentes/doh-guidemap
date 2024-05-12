a99e86ab6e4bc926a2cbd3c998f0356cb71ef0c0
Jead Fuentes Personal <jeadfuentes.17@gmail.com>
2024-05-12 23:18:10 +0800

    added zoom in image

--- src/Pages/1st/Hrmo.jsx
+++ src/Pages/1st/Hrmo.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faPlus, faMinus, faRotateLeft } from '@fortawesome/free-solid-svg-icons'
+import { TransformWrapper, TransformComponent, useControls } from 'react-zoom-pan-pinch'
 
 function Hrmo() {
   return (
     <>
+    <body>
     <div className="fixed top-0 left-0 p-4">
         <button className="bg-amber-100 border-2 border-amber-600 text-black text-md px-10 rounded-md w-40 h-10 flex flex-row items-center justify-center">
-        <Link to="/first/"><FontAwesomeIcon icon={faArrowLeft} /> Back</Link>
+        <Link to="/first"><FontAwesomeIcon icon={faArrowLeft} /> Back</Link>
         </button>
     </div>
-    <div className='place-self-center text-2xl text-center bg-green-100'>
-        <img src="/floor-images/1/hrmo.png" alt="Records" />
+    <div className='place-self-center text-2xl text-center mx-auto ml-56' style={{width: "80%"}}>
+      <TransformWrapper defaultScale={1} defaultPositionX={100} defaultPositionY={100}>
+          <TransformComponent>
+              <img src="/floor-images/1/hrmo.png" alt="Records" style={{width: "85%"}}/>
+          </TransformComponent>
+        </TransformWrapper>
     </div>
+    </body>
     </>
   )
 }
